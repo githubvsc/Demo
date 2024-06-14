3067535b7d76611ee30bc982a1e89688fc702eb3
sj1519 <jadhavsagar7865@gmail.com>
2024-06-14 12:20:15 +0530

    h1 tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './pages/login/dashboard/dashboard.component';
+import { LayoutComponent } from './pages/login/layout/layout.component';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [HomeComponent, LoginComponent,RouterOutlet,LoginComponent,LayoutComponent,DashboardComponent]
 })
 export class AppComponent {
   title = 'Demo';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/login/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/login/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', redirectTo: 'login', pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ]
+  }
+];
